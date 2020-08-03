c4f633535ddb33e0d920603d96d547e70ff77728
Payton Gray <paytongray18@gmail.com>
2020-08-03 20:39:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,7 +123,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
-end
-programmer_hash[:dennis_ritchie][:languages][0]
+programmer_hash[:dennis_ritchie][:languages]
 end
