ab88f63e54b4c267eb849f2d8ea26b0783e46a60
Payton Gray <paytongray18@gmail.com>
2020-08-03 21:10:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,11 +78,6 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
- 		}
-      programmer_hash[:yukihiro_matsumoto] = {}
-        programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-        programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-      programmer_hash
     }
 end
 
